262913c0f55b8394c14657dca83151b7f6358fae
sammy freddie <sammyfreddie@sammys-MacBook-Pro.local>
2022-10-17 15:35:11 -0700

    initial4

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import NavigationIcon from '@mui/icons-material/Navigation';
 import { Outlet } from "react-router-dom";
 
 export default function Root(){

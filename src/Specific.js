8ff673d9051800871fd6d7f382f6b55e7122cb68
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-10-16 20:26:38 -0700

    initial10

--- src/Specific.js
+++ src/Specific.js
@@ -1,20 +1,38 @@
-import {fetchUnpaidObjArraySpecific} from "./dataFetching.mjs";
+import {fetchPropertyInfoObj, fetchUnpaidObjArray, fetchUnpaidObjArraySpecific} from "./dataFetching.mjs";
 import react, {useState} from "react";
+import NavLinks from "./NavLinks";
+import {Link, useParams} from "react-router-dom";
 
 export default function Specific(){
 
-    const [specific, setSpecific ] = useState()
+    const {id} = useParams()
+    const [unpaidObjArray, setUnpaidObjArray] = useState([])
+
+    const [specific, setSpecific ] = useState([{name:"", url:""}])
 
     react.useEffect(()=>{
         fetchUnpaidObjArraySpecific().then(x=> {
             setSpecific(...x);
-        })
-    })
+        });
+        fetchUnpaidObjArray().then( unpaidObjArray => {
+                var objArray = [];
+                objArray.push(...[{name: "<-----", url: "/"}, {name: "", url: "/autopay"}]);
+                for(let i = 0; i < unpaidObjArray.length; i++){
+                    if (unpaidObjArray[i].url == id) {
+                        objArray.push({name:unpaidObjArray[i].name + ": $" + unpaidObjArray[i].amount, url:"/"});
+                        break;
+                    }
+                    objArray.push(...[{name:"", url:""}])
+                }
+
+                setUnpaidObjArray(objArray);
+            }
+        );
+})
 
     return(
         <div>
-
-            <h1>{specific.name}</h1>
+            <NavLinks objArry ={unpaidObjArray}/>
         </div>
     )
 }

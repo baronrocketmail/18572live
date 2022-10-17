e03650cd55ed294eacc7219b81030e1b43b10ae1
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-10-17 06:00:59 -0700

    initial13

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,13 @@
 import {fetchPropertyInfoObj, fetchUnpaidObjArray} from "./dataFetching.mjs";
 import NavLinks from "./NavLinks";
 import react, {useState} from "react";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import NavigationIcon from '@mui/icons-material/Navigation';
 
 export default function Home(){
     const [unpaidObjArray, setUnpaidObjArray] = useState([])
@@ -22,6 +29,12 @@ export default function Home(){
     return(
         <div>
         <NavLinks objArry = {unpaidObjArray}/>
+            <Box className={"customize"} sx={{ '& > :not(style)': { m: 1 } }}>
+                <Fab variant="extended">
+                    <NavigationIcon sx={{ mr: 1 }} />
+                    Customize
+                </Fab>
+            </Box>
         </div>
     )
 }

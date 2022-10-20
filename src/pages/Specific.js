c67547d8c53e4e4f096d7da5f9d2855c384160b1
sammy freddie <sammyfreddie@unknownf4d4887bc1bf.attlocal.net>
2022-10-20 07:19:56 -0700

    initial

--- src/pages/Specific.js
+++ src/pages/Specific.js
@@ -1,11 +1,12 @@
-import {fetchPropertyInfoObj, fetchUnpaidObjArray, fetchUnpaidObjArraySpecific} from "../api/dataFetching.mjs";
+import {fetchUnpaidObjArray, fetchUnpaidObjArraySpecific} from "../api/dataFetching.mjs";
 import NavLinks from "../components/NavLinks";
-import {Link, useParams} from "react-router-dom";
-import React, {useState, useEffect, useContext} from "react";
+import {useParams} from "react-router-dom";
+import React, {useState} from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "../components/CheckoutForm";
-import "../styles/App.css";
+import "../styles/paymentElement.css";
+
 import react from "react";
 
 const stripePromise = loadStripe("pk_live_51LlESTC3Ie0MSAM2CQtveok1BNyKHlkw8W0aVunFTMYjMAGi0y6dEaHreNGy0TC4oRkfSMwOkcXUftn0oTlwDaBg00bnHjzls6");
@@ -38,7 +39,7 @@ export default function Specific(){
                 var objArray = [];
                 objArray.push(...[{name: "<-----", url: "/"}, {name: "", url: "/autopay"}]);
                 for(let i = 0; i < unpaidObjArray.length; i++){
-                    if (unpaidObjArray[i].url == id) {
+                    if (unpaidObjArray[i].url === id) {
                         objArray.push({name:unpaidObjArray[i].name + ": $" + unpaidObjArray[i].amount, url:"/"});
                         break;
                     }
